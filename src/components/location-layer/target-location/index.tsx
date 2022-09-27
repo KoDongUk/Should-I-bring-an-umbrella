b9d8be362235f4130df0c51b8998b306e92cbb39
kodonguk <kodonguk@wemakeprice.com>
2022-09-27 11:33:31 +0900

    feature: 컴포넌트 초기 생성

---
+++ src/components/location-layer/target-location/index.tsx
@@ -0,0 +1,4 @@
+function Target() {
+  return;
+}
+export default Target;
---
+++ src/pages/MainLayerComponent/index.tsx
@@ -0,0 +1,9 @@
+import header from "@/components/header";
+import home from "@/components/location-layer/home-location";
+import target from "@/components/location-layer/target-location";
+
+function MainLayerComponent() {
+  return;
+}
+
+export default MainLayerComponent;
